1585bee25fd9f30d661c0531846471ec805e73d7
s2pid0 <gorbovsergei@gmail.com>
2021-12-30 00:32:10 +0300

    Initial commit

---
+++ src/com/company/Dog.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Dog {
+
+    String barking;
+    boolean howling;
+    int whining;
+
+
+    public static String bark(String barking){
+        barking = barking;
+        return barking;
+    }
+
+    public static boolean bark(boolean howling){
+        howling = howling;
+        return howling;
+    }
+
+    public static int bark(int whining){
+        whining = whining;
+        return whining;
+    }
+}
---
+++ src/com/company/First.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class First {
+
+    String one;
+    int second;
+    boolean third;
+
+    public First(String one){
+        this.one = one;
+    }
+
+    public First(int second){
+        this.second = second;
+    }
+
+    public First(boolean third){
+        this.third = third;
+    }
+
+}
